eb0ddbf6a22fc906b7fae710d4e5e10eb2f7a9cf
Júlio Santos <juliorasantos@gmail.com>
2025-08-12 19:01:23 -0300

    Footer, body e css
    Fiz o footer, body e arrumei o css para ficar tudo no lugar.

--- santos-app/src/app/app.ts
+++ santos-app/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainHeader } from './main-header/main-header';
+import { IntroMain } from './intro-main/intro-main';
+import { MainFooter } from './main-footer/main-footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainHeader],
+  imports: [RouterOutlet, MainHeader, IntroMain, MainFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ santos-app/src/app/intro-main/intro-main.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-main',
+  imports: [],
+  standalone: true,
+  templateUrl: './intro-main.html',
+  styleUrl: './intro-main.scss'
+})
+export class IntroMain {
+
+}
