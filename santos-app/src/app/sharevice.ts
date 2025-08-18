67e45b18fdfa58145dc01453c498d1a198301df5
Júlio Santos <juliorasantos@gmail.com>
2025-08-18 14:07:05 -0300

    Connect db e service
    Conectado ao db e feito as conexões

--- santos-app/src/app/sharevice.ts
+++ santos-app/src/app/sharevice.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Shares } from './shares'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class Sharevice {
-  
+
+
+  constructor(private firestore: Firestore) {}
+
+  getShares(): Observable<Shares[]> {
+
+    const sharesCollection = collection(this.firestore, 'projetos');
+
+    return collectionData(sharesCollection, { idField: 'id' }) as Observable<Shares[]>;
+  }
 }
--- santos-app/src/main.ts
+++ santos-app/src/main.ts
@@ -3,9 +3,11 @@ import { importProvidersFrom } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(App, {
   providers: [
-    importProvidersFrom(RouterModule.forRoot(routes))
+    importProvidersFrom(RouterModule.forRoot(routes)), provideFirebaseApp(() => initializeApp({ projectId: "spp-1701", appId: "1:150001021065:web:917619fd6af4bf21bd1658", storageBucket: "spp-1701.firebasestorage.app", apiKey: "AIzaSyC3x4OGYkBZBl42YGKssfzaPwcMlUck_NU", authDomain: "spp-1701.firebaseapp.com", messagingSenderId: "150001021065", measurementId: "G-PJMGHHM4LE" })), provideFirestore(() => getFirestore())
   ]
 }).catch(err => console.error(err));
