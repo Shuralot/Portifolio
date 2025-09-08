cf407136bd5e4af2ecbbf9ec96594800f87c018d
Júlio Santos <juliorasantos@gmail.com>
2025-09-08 13:40:42 -0300

    Add responsive main header with mobile support
    Introduces mobile responsiveness to the main header using Angular CDK BreakpointObserver. Updates HTML to provide separate layouts for desktop and mobile, including a hamburger menu for mobile navigation. Adds @angular/cdk as a dependency and refactors related styles and TypeScript logic for device detection.

--- src/app/main-header/main-header.ts
+++ src/app/main-header/main-header.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-header',
-  imports: [ RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './main-header.html',
   styleUrls: ['./main-header.scss'],
   standalone: true,
 })
 export class MainHeader {
 
-
+  isMobile=false;
+  
+   constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .subscribe((result: BreakpointState) => {
+        // Essa é a forma mais confiável
+        this.isMobile = result.matches;
+      });
+  }
 }
